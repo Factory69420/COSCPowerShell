9e29694397b90a138bb4f58b657bf4119dd43b4f
Nothanks2003 <39002902+Factory69420@users.noreply.github.com>
2024-05-20 09:18:32 -0400

    Update Main_notes.ps1

--- Main_notes.ps1
+++ Main_notes.ps1
@@ -1,6 +1,33 @@
+ssh student@10.50.X.X -X (enter ips for X.X)
+ssh student@10.50.32.21 -X (Windows)
+ssh student@10.50.38.145 -X (Linux Use terminator command to open weird cmd)
+Terminator
+
+http://10.50.24.190/challenges
+LCplElliott
+passwd: Carmel69420!
+xfreerdp /u:student /v:10.50.32.21 -dynamic-resolution +glyph-cache +clipboard
+
+
 -----------------------------------------------------------------------------DAY 1--------------------------------------------------------------
+>> $false and $true
+important variables
+>> $_ (pipeline var)
+>> $Mathces
+>> $Input
+!TYPECASTING
+([string]$false + 'hello world')
+[int]
+[float]
+[array]
 
 
 
 
+$true + 'hello world'
 
+
+METHODS:
+>> .GetType()
+>> .Name (doesnt print table just prints type USEFUL!!!)
+>> | get-member 
